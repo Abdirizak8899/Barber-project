bbf3c5b055f97e892c2511b9cd3e8cfd79e9bd0b
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2023-01-11 13:11:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const show = () =>{
+    let x = document.getElementById("menue")
+  
+    if (x.style.display === 'flex'){
+      x.style.display = 'none'
+    }
+    else{
+      x.style.display = 'flex'
+    }
+  }
